579f60acd138c5b67d46798c1a80c2840516fe04
Matheus Ferraria <matheusaf8@gmail.com>
2022-02-25 17:15:34 -0300

    adicionando comentarios

--- backend/src/main.go
+++ backend/src/main.go
@@ -10,29 +10,41 @@ import (
 )
 
 func main() {
+	// inicializando o servidor
 	router := gin.Default()
 
+	// criando grupo de rotas
 	v1 := router.Group("/v1")
 	{
+		// criando rota para receber a mensage
+		// esta funcao recebe um POST request contendo a mensagem no body
 		v1.POST("/message", func(c *gin.Context) {
 			var message model.MessageModel
 
+			// realizando o parse do json para o modelo de mensagem
+			// caso apresente algum erro o servidor retornara 400 com a mensagem informando o erro
 			if err := c.ShouldBindJSON(&message); err != nil || message.Channel == "" || message.Message == "" {
 				c.JSON(http.StatusBadRequest, view.ErrorMessage{
 					Message: "Mensagem recebida apresenta um formato inválido ou não apresenta valores",
 					Error:   err.Error()})
+				// retorna antes para evitar a execucao do bloco abaixo
 				return
 			}
 
+			// Realizando o envio da mensagem para a fila e armazenando a resultado e o erro retornado
 			msgOutput, err := sqs.SendMessage(message)
 
+			// Caso tenha ocorrido algum erro ao enviar a mensagem o servidor ira retornar 500 com a mensagem de erro
 			if err != nil {
 				c.JSON(http.StatusInternalServerError, view.ErrorMessage{
 					Message: "Erro ao enviar mensagem",
 					Error:   err.Error()})
+				// retorna antes para evitar a execucao do bloco abaixo
 				return
 			}
 
+
+			// Caso o model recebido esteja correto e a mensagem seje enviada com sucesso o servidor retornara 200 com os itens da mensagem
 			c.JSON(http.StatusOK, view.MessageView{
 				MessageId:         *msgOutput.MessageId,
 				MessageBody:       *msgOutput.MD5OfMessageBody,
@@ -41,5 +53,6 @@ func main() {
 		})
 	}
 
+	// executando o servidor na porta 80
 	router.Run(":80")
 }
--- backend/src/sqs/sqs.go
+++ backend/src/sqs/sqs.go
@@ -2,7 +2,6 @@ package sqs
 
 import (
 	"context"
-	"fmt"
 	"os"
 
 	"teste_ifood/model"
@@ -13,8 +12,10 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 )
 
+// Funcao responsavel por criar a comunicacao com o cliente do SQS
 func GetClient(endppoint string, signingRegion string) (*sqs.Client, error) {
 
+	// Funcao para alterar o endpoint utilizado pelo AWS CLI
 	endPointResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
 		if endppoint != "" {
 			return aws.Endpoint{
@@ -23,36 +24,45 @@ func GetClient(endppoint string, signingRegion string) (*sqs.Client, error) {
 				SigningRegion: signingRegion,
 			}, nil
 		}
-		fmt.Println("Endpoint: ", endppoint)
 		return aws.Endpoint{}, &aws.EndpointNotFoundError{}
 	})
 
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"), config.WithEndpointResolverWithOptions(endPointResolver))
+	// Carrega as configuracoes utilizando a funcao de configuracao definida anteriormente
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithEndpointResolverWithOptions(endPointResolver))
 
+	// Caso ocorra algum erro, retorna o erro e o cliente retornara nil
 	if err != nil{
 		return nil, err
 	}
 
+	// Cria e retorna o cliente do SQS
 	return sqs.NewFromConfig(cfg), err
 }
 
+// Funcao resposavel por obter a url da fila
 func GetQueue(queueName string, client *sqs.Client) (*string, error) {
 
+	// Cria o objeto para pesquisar a fila utilizando o nome da fila
 	getQueueUrl := &sqs.GetQueueUrlInput{
 		QueueName: aws.String(queueName),
 	}
 
+	// Funcao responsavel por obter a url da fila utilizando o objeto criado anteriormente
 	result, err := client.GetQueueUrl(context.TODO(), getQueueUrl)
 
+	// Caso ocorra algum erro, retorna o erro e a url retornara nil
 	if err != nil{
 		return nil, err
 	}
 
+	// Retorna a url da fila
 	return result.QueueUrl, err
 }
 
+// Funcao responsavel por enviar a mensagem
 func SendMessageHelper(message model.MessageModel, client *sqs.Client, queueUrl string) (*sqs.SendMessageOutput, error) {
 
+	// Criando o objeto que representa a mensagem a ser enviada
 	sqsMessage := &sqs.SendMessageInput{
 		DelaySeconds: 10,
 		MessageAttributes: map[string]types.MessageAttributeValue{
@@ -68,34 +78,44 @@ func SendMessageHelper(message model.MessageModel, client *sqs.Client, queueUrl 
 		QueueUrl: aws.String(queueUrl),
 	}
 
+	// Funcao que envia a mensagem para a fila
 	resp, err := client.SendMessage(context.TODO(), sqsMessage)
 
+	// Caso ocorra algum erro, retorna o erro e a mesangem retornara nil
 	if err != nil{
 		return nil, err
 	}
 
+	// Retorna a resposta do SQS apos receber a mensagem
 	return resp, err
 }
 
+
+// Funcao consolidada que ira realizar o envio da mensagem utilizando 
+// de apoio todas as funcoes definidas anteriormente
 func SendMessage(message model.MessageModel) (*sqs.SendMessageOutput, error) {
 
+	// Obtem as variaveis de ambiente necessarias para configurar o cliente do SQS
 	AWS_ENDPOINT := os.Getenv("AWS_ENDPOINT")
 	SQS_QUEUE_NAME := os.Getenv("SQS_QUEUE_NAME")
 	AWS_DEFAULT_REGION := os.Getenv("AWS_DEFAULT_REGION")
 
+	// Chamando a funcao definida anteriormente para criar e obter o cliente do SQS
 	client, errorClient := GetClient(AWS_ENDPOINT, AWS_DEFAULT_REGION)
 
+	// Retorna antes caso tenha obtido algum erro ao criar o cliente
 	if errorClient != nil {
 		return nil, errorClient
 	}
-
-	queue_url, errorUrl := GetQueue(SQS_QUEUE_NAME, client)
 	
+	// Chamando a funcao definida anteriormente para obter a url da fila
+	queue_url, errorUrl := GetQueue(SQS_QUEUE_NAME, client)
+
+	// Retorna antes caso tenha obtido algum erro ao obter a url da fila
 	if errorUrl != nil {
 		return nil, errorUrl
 	}
 	
-		fmt.Println(*queue_url)
-
+	// Retorna o resultado da funcao de apoio para enviar a mensagem
 	return SendMessageHelper(message, client, *queue_url)
 }
